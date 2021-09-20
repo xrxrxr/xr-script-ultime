3f6e811956225f46c10e827d8ae7a35f7840ad48
Xavier RE4 <xrthefrench@gmail.com>
2021-09-20 12:09:01 +0200

    noto font burmese/ japan ok

--- appli.sh
+++ appli.sh
@@ -17,6 +17,14 @@ sudo apt install stow -y
 sudo apt install redshift-gtk -y
 # fonts notofu to have chinese japanese and corean 
 sudo apt install fonts-noto-cjk-extra fonts-noto-cjk -y
+sudo apt install xfonts-intl-asian -y
+sudo apt install xfonts-thai -y
+sudo apt install fonts-noto-core -y
+sudo apt install fonts-noto-extra -y
+sudo apt install fonts-noto-ui-core -y
+sudo apt install fonts-noto-ui-extra -y
+
+
 ## chrome install 
 #wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 #sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
